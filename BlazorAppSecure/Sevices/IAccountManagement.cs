9047d1051a6ab0fed71d6ee4d7dd5e6240ac102c
kartik <kartik.kumar53@gmail.com>
2024-04-11 16:39:15 +0530

    Add project files.

---
+++ BlazorAppSecure/Sevices/IAccountManagement.cs
@@ -0,0 +1,20 @@
+﻿using BlazorAppSecure.Model;
+using System.Data;
+
+namespace BlazorAppSecure.Sevices
+{
+    public interface IAccountManagement
+    {
+        public Task<FormResult> RegisterAsync(string email, string password);
+
+        public Task<FormResult> LoginAsync(string email, string password);
+
+        public Task LogoutAsync();
+
+        public Task<bool> CheckAuthenticatedAsync();
+
+        public Task<List<Role>> GetRoles();
+
+        public Task<FormResult> AddRole(string[] roles);
+    }
+}
